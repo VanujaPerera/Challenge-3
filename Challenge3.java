ddc8669dea61f9319fe6ab01ef080afe370cf1bf
Vanuja Perera <21002553@student.curtin.edu.au>
2022-12-22 23:19:33 +0530

    Output modified to increase visibility

--- Challenge3.java
+++ Challenge3.java
@@ -8,11 +8,9 @@ public class Challenge3 {
 
             for (int j = i + 1; j < arr.length; j++) {
 
-                // Checking elements
                 int temp = 0;
                 if (arr[j] < arr[i]) {
 
-                    // Swapping
                     temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
